6f66d62a3994c7193ba95627c218caf6ef56b7f2
Fabián Araya Ortega <35875278+Mashtic@users.noreply.github.com>
2023-11-10 19:40:57 -0600

    Documemto del proyecto
    Doocumento de la Interfaz

---
+++ proyecto2Bases_SistemaDeMarcasCorp_FabianIanJoselynJeison/src/main/java/com/mycompany/proyecto2bases_sistemademarcascorp_fabianianjoselynjeison/Proyecto2Bases_SistemaDeMarcasCorp_FabianIanJoselynJeison.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.proyecto2bases_sistemademarcascorp_fabianianjoselynjeison;
+
+/**
+ *
+ * @author fabia
+ */
+public class Proyecto2Bases_SistemaDeMarcasCorp_FabianIanJoselynJeison {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
